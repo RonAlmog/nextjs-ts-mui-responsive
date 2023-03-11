65b42bd3ad603c1fcc7d507d2ebaa621d69cb729
Ron Almog <ron8132@gmail.com>
2023-03-11 09:22:07 -0500

    some more button tricks

--- pages/buttons.tsx
+++ pages/buttons.tsx
@@ -1,13 +1,34 @@
 import Layout from "@/components/layout/Layout";
-import React from "react";
-import { Box, Stack, Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  Stack,
+  Button,
+  IconButton,
+  ButtonGroup,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@mui/material";
 import CopyIcon from "@mui/icons-material/FileCopyOutlined";
 import { PrintRounded, Share } from "@mui/icons-material";
 import EditIcon from "@mui/icons-material/Edit";
+import {
+  Send,
+  FormatBold,
+  FormatItalic,
+  FormatUnderlined,
+} from "@mui/icons-material";
 
 type Props = {};
 
 const SpeedDialPage = (props: Props) => {
+  const [formats, setFormats] = useState<string[]>([]);
+  const handleFormatChange = (
+    e: React.MouseEvent,
+    updatedFormats: string[]
+  ) => {
+    setFormats(updatedFormats);
+  };
   return (
     <Layout>
       <Stack spacing={3}>
@@ -35,6 +56,63 @@ const SpeedDialPage = (props: Props) => {
           <Button variant="contained" color="success">
             Success
           </Button>
+        </Stack>
+        <Stack display="block" direction="row" spacing={3}>
+          <Button variant="text" href="https://google.ca">
+            Google
+          </Button>
+          <Button variant="contained" size="small" color="primary">
+            Contained
+          </Button>
+          <Button variant="outlined" size="medium" color="secondary">
+            Outlined
+          </Button>
+          <Button
+            variant="contained"
+            size="large"
+            color="warning"
+            disableElevation
+          >
+            Warning
+          </Button>
+        </Stack>
+        <Stack display="block" direction="row" spacing={3}>
+          <Button variant="contained" color="error" startIcon={<Send />}>
+            Error
+          </Button>
+          <Button variant="contained" color="success" endIcon={<Send />}>
+            Success
+          </Button>
+          <IconButton color="success" size="small">
+            <Send />
+          </IconButton>
+        </Stack>
+
+        <Stack direction="row">
+          <ButtonGroup variant="contained">
+            <Button>Left</Button>
+            <Button>Center</Button>
+            <Button>Right</Button>
+          </ButtonGroup>
+        </Stack>
+        <Stack direction="row">
+          <ToggleButtonGroup
+            aria-label="xx"
+            size="medium"
+            color="success"
+            value={formats}
+            onChange={handleFormatChange}
+          >
+            <ToggleButton value="Bold">
+              <FormatBold />
+            </ToggleButton>
+            <ToggleButton value="Italic">
+              <FormatItalic />
+            </ToggleButton>
+            <ToggleButton value="Underline">
+              <FormatUnderlined />
+            </ToggleButton>
+          </ToggleButtonGroup>
         </Stack>
       </Stack>
     </Layout>
