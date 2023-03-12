a7bf35b70f1ed3f0fdb89503359eaf9cd3b37fce
Ron Almog <ron8132@gmail.com>
2023-03-12 12:53:13 -0400

    paper

--- pages/grid.tsx
+++ pages/grid.tsx
@@ -1,34 +1,37 @@
 import Layout from "@/components/layout/Layout";
 import React, { useState } from "react";
-import { Box, Divider, Grid, Stack } from "@mui/material";
+import { Box, Divider, Grid, Paper, Stack } from "@mui/material";
+import { margin } from "@mui/system";
 
 type Props = {};
 
 const GridPage = (props: Props) => {
   return (
     <Layout>
-      <Grid container my={4} spacing={2}>
-        <Grid item xs={12} sm={6} lg={3}>
-          <Box bgcolor="primary.light" p={2}>
-            Iten 1
-          </Box>
+      <Paper sx={{ padding: "32px", margin: "10px" }} elevation={4}>
+        <Grid container my={4} spacing={2}>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Box bgcolor="primary.light" p={2}>
+              Iten 1
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Box bgcolor="primary.light" p={2}>
+              Iten 2
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Box bgcolor="primary.light" p={2}>
+              Iten 3
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Box bgcolor="primary.light" p={2}>
+              Iten 4
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <Box bgcolor="primary.light" p={2}>
-            Iten 2
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <Box bgcolor="primary.light" p={2}>
-            Iten 3
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <Box bgcolor="primary.light" p={2}>
-            Iten 4
-          </Box>
-        </Grid>
-      </Grid>
+      </Paper>
     </Layout>
   );
 };
