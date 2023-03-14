d1e50c1a637468b65b028b07bc56968dbea676f2
Ron Almog <ron8132@gmail.com>
2023-03-14 09:29:45 -0400

    feedback page

---
+++ pages/feedback.tsx
@@ -0,0 +1,22 @@
+import {
+  Box,
+  Button,
+  Drawer,
+  Paper,
+  Switch,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { MenuBook } from "@mui/icons-material";
+import Link from "../src/Link";
+import { useState } from "react";
+import Layout from "@/components/layout/Layout";
+import { Stack } from "@mui/system";
+type Props = {};
+
+const DrawerPage = (props: Props) => {
+  const [drawerOpen, setdrawerOpen] = useState(false);
+  return <Layout>hh</Layout>;
+};
+
+export default DrawerPage;
